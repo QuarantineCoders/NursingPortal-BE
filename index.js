585aa4f153ea170341de6f4596dde0e02f52e29c
Kugan <kugapriyanj97@gmail.com>
2024-06-27 03:32:13 +0530

    added home api

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the API" });
+});
+
 app.use("/api/", routes);
 
 app.use(errorHandler);
